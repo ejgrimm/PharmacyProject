9db89afddc7cd7dfc7b2d3724cca4cd4e15e3595
anthonygreen <anthonygreen@10.31.39.75>
2019-04-15 15:57:18 -0400

    pharmacist uwu

--- src/Pharmacist.java
+++ src/Pharmacist.java
@@ -1,4 +1,55 @@
 
-public class Pharmacist {
+public class Pharmacist implements PrescriptionFillable {
 
+	//datamembers
+	private String name;
+	private String ssn;
+	private String certificationDate;
+	
+	public Pharmacist() {
+		name = "not found";
+	}
+	
+	public Pharmacist(String n, String s, String c) {
+		
+	}
+
+	//getters and setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getCertificationDate() {
+		return certificationDate;
+	}
+
+	public void setCertificationDate(String certificationDate) {
+		this.certificationDate = certificationDate;
+	}
+	
+	//other methods
+	public boolean fillPrescription(Prescription rx) {
+		return false;
+	}
+	
+	public void requestRefill(DrugLine dl) {
+		
+	}
+	
+	public boolean checkContraindications(Drug d) {
+		return false;
+	}
+	
 }
