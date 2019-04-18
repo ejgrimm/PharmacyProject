e95e8ed5faa4de4a7fad7f154223a2203cf8852a
alici <alici@10.31.58.182>
2019-04-18 13:45:49 -0400

    Driver update

--- src/PharmacyDriver.java
+++ src/PharmacyDriver.java
@@ -3,6 +3,7 @@ public class PharmacyDriver {
     public static void main(String[] args) {
         
         // putting methods to instantiate objects from files in each Class
+        // create an arraylist of Class Objects from reading every file in driver
         
     }
 }
