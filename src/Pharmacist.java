588988fcab0cecfb0eb9813566c87942f7e29536
anthonygreen <anthonygreen@10.31.33.183>
2019-04-28 13:33:17 -0400

    fill rx progress :(

--- src/Pharmacist.java
+++ src/Pharmacist.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Pharmacist extends Person {
 
@@ -23,12 +24,27 @@ public class Pharmacist extends Person {
 	}
 	
 	//other methods
-	public boolean fillPrescription(Prescription rx, Person p) {
-		return false;
+	public boolean fillPrescription(Prescription rx, Person p, ArrayList<Drug> drugs, ArrayList<Patient> patients, ArrayList<Doctor> doctors) {
+		//check if authorized
+		if (p instanceof OutPatient || p instanceof Nurse) {
+			//check if prescription is currently in the system
+			Patient patient = rx.getPatient();
+			if (rx.getSetOfDrugLines().contains(patient.getCurrentPrescription())) {
+				//update
+				
+			}
+			else {
+				//add new drugline to Patient object and update file
+			}
+				
+		}
+		else
+			return false;
+			System.out.println("Not authorized to fill out this prescription");
 	}
 	
 	public boolean checkContraindications(Drug d) {
-		return false;
+		
 	}
 	
 }
